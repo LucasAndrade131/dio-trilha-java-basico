beb43bf1deed3768569ad4e00e03cd0d7097b67d
LucasAndrade131 <lucas.andrade131@outlook.com>
2024-05-02 12:29:11 -0300

    First Commit

---
+++ java-anatomia-classes/src/edu/lucas/primeirasemana/DeclararMetodos.java
@@ -0,0 +1,15 @@
+package edu.lucas.primeirasemana;
+
+public class DeclararMetodos {
+    public static void main(String[] args) {
+
+        String primeiroNome = "Lucas";
+        String segundoNome = "Andrade";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);//chamando o metodo nomeCompleto
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) { //método
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
